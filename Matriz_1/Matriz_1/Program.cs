57e31a806912622f171d6de627b3e5baf56775ab
Airon Lima Selistre <farbyswog@hotmail.com>
2020-12-28 22:27:38 -0300

    added Matriz_1 and Matriz_2

---
+++ Matriz_1/Matriz_1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Matriz_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Ler dois números M e N, e depois ler uma matriz MxN de números inteiros, conforme exemplo.
+             * Em seguida, mostrar na tela somente os números negativos da matriz.
+             * 
+             * Entrada: A entrada contém os números M e N na mesma linha, depois os dados da matriz.
+             * Saída: A saída contém uma mensagem e depois os números negativos da matriz, conforme exemplo. 
+            */
+
+            // Definindo tamanho da matriz
+            string[] entrada = Console.ReadLine().Split(' ');
+
+            // Guardando tamanho de linhas e colunas
+            int M = int.Parse(entrada[0]);
+            int N = int.Parse(entrada[1]);
+
+            // Criando e instanciando matriz
+            int[,] mat = new int[M, N];
+
+            // Guardando valores na matriz
+            for (int i=0; i<M; i++)
+            {
+                string[] s = Console.ReadLine().Split(' '); 
+                for (int c=0; c<N; c++)
+                {
+                    mat[i, c] = int.Parse(s[c]);
+                }
+            }
+
+            Console.WriteLine("VALORES NEGATIVOS:");
+
+            // Identificando números negativos
+            for (int i=0; i<M; i++)
+            {
+                for (int c=0; c<N; c++)
+                {
+                    if (mat[i, c] < 0)
+                    {
+                        Console.WriteLine(mat[i, c]);
+                    }
+                }
+            }
+ 
+        }
+    }
+}
---
+++ Matriz_2/Matriz_2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Matriz_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Ler um número N e depois uma matriz quadrada NxN com números inteiros. Depois, mostrar na tela a soma dos elementos de cada linha da matriz. 
+             * 
+             * Entrada:
+             * A entrada contém números representando a soma dos elementos de cada linha da matriz.
+             * 
+             * Saída:
+             * A saída contém os números representando a soma dos elementos de cada linha da matriz.
+            */
+
+            // Definindo tamanho matriz
+            int N = int.Parse(Console.ReadLine());
+
+            // Criando e instanciando matriz
+            int[,] mat = new int[N, N];
+
+            // Lendo e guardando valores na matriz
+            for (int i=0; i<N; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                for (int c=0; c<N; c++)
+                {
+                    mat[i, c] = int.Parse(s[c]);
+                }
+            }
+
+            // Soma dos elementos de cada linha da matriz
+            int[] soma = new int[N];
+
+            for (int i=0; i<N; i++)
+            {
+                for (int c=0; c<N; c++)
+                {
+                    soma[i] += mat[i, c];
+                }
+                Console.WriteLine(soma[i]);
+            }
+        }
+    }
+}
